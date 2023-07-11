d26d4fafe47d50a3c5966960aedb61e8bad2193a
maciekglowka <maciek.glowka@gmail.com>
2023-07-11 13:27:30 +0200

    initial maxlen v1 fix

--- tests/test_v1.py
+++ tests/test_v1.py
@@ -1,3 +1,6 @@
+from collections import deque
+from typing import Deque
+
 from pydantic import VERSION
 from pydantic.v1 import VERSION as V1_VERSION
 from pydantic.v1 import BaseModel as V1BaseModel
@@ -21,3 +24,22 @@ def test_root_validator():
 
     model = Model(v='value')
     assert model.v == 'value-v1'
+
+
+def test_deque_maxlen():
+    class DequeTypedModel(V1BaseModel):
+        field: Deque[int] = deque(maxlen=10)
+
+    assert DequeTypedModel(field=deque(maxlen=25)).field.maxlen == 25
+    assert DequeTypedModel().field.maxlen == 10
+
+    class DequeUnTypedModel(V1BaseModel):
+        field: deque = deque(maxlen=10)
+
+    assert DequeUnTypedModel(field=deque(maxlen=25)).field.maxlen == 25
+    assert DequeTypedModel().field.maxlen == 10
+
+    class DeuqueNoDefaultModel(V1BaseModel):
+        field: deque
+
+    assert DeuqueNoDefaultModel(field=deque(maxlen=25)).field.maxlen == 25
